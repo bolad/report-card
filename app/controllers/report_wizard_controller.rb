8523281fc5e31842f036af9f4d97f82d42b191aa
stanley akyea <stanley.akyea@gmail.com>
2021-06-09 17:55:04 +0200

    Add progress bar to wizard steps

--- app/controllers/report_wizard_controller.rb
+++ app/controllers/report_wizard_controller.rb
@@ -1,6 +1,6 @@
 class ReportWizardController < ApplicationController
   include Wicked::Wizard
-  #before_action :set_progress, only: [:show]
+  before_action :set_progress, only: [:show]
 
   steps :project_information, :executive_summary
 
@@ -15,4 +15,15 @@ class ReportWizardController < ApplicationController
   def finish_wizard_path
     root_path
   end
+
+  private 
+
+  def set_progress
+    if wizard_steps.any? && wizard_steps.index(step).present?
+      @progress = ((wizard_steps.index(step) + 1).to_d / wizard_steps.count.to_d) * 100
+    else
+      @progress = 0
+    end
+  end
+
 end
