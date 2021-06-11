a3d51d6f3fd08ae6423b15cfe07d714eeba2b871
stanley akyea <stanley.akyea@gmail.com>
2021-06-11 23:21:47 +0200

    Add rich text area to report fields

--- app/models/report.rb
+++ app/models/report.rb
@@ -5,4 +5,7 @@ class Report < ApplicationRecord
   has_rich_text :executive_summary
   has_rich_text :project_background
   has_rich_text :project_objective
+  has_rich_text :proposed_developments
+  has_rich_text :catchment_details
+  has_rich_text :conclusion
 end
