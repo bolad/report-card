d0935215b8c5968bfe23477526dadcf5a3f684c2
stanley akyea <stanley.akyea@gmail.com>
2021-06-14 21:58:33 +0200

    Integrate cocoon for adding and removing hidden fields

--- app/controllers/reports/report_wizard_controller.rb
+++ app/controllers/reports/report_wizard_controller.rb
@@ -63,7 +63,6 @@ class Reports::ReportWizardController < ApplicationController
     params.require(:report).permit(:project_name, :project_number, :document_title, :document_number, :document_status, :date, 
                                   :client_name, :client_number, :project_manager, :author, :checked_by, :description, :executive_summary, 
                                   :project_background, :project_objective, :proposed_developments, :catchment_details, :data_assessment, :conclusion, 
-                                  executives_attributes: [:project_name, :project_number, :potential_impact, :date])
+                                  executives_attributes: [:id, :project_name, :project_number, :potential_impact, :date, :_destroy])
   end
-
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -2,7 +2,9 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :executives
   validates :project_name, presence: true
-  accepts_nested_attributes_for :executives
+  accepts_nested_attributes_for :executives,
+                                allow_destroy: true,
+                                reject_if: lambda {|attrs| attrs['project_name', 'project_number', 'potential_impact', 'date'].blank? }
 
   has_rich_text :executive_summary
   has_rich_text :project_background
