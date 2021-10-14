08a1940000b4df66971f1eeb0d089dc1f13f63f2
stanley akyea <stanley.akyea@gmail.com>
2021-10-14 13:34:14 +0200

    Adjust bg image size of report

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -23,7 +23,7 @@ class ReportsController < ApplicationController
     @report.user = current_user
     respond_to do |format|
       if @report.save
-        format.html { redirect_to report_report_wizard_index_path(@report), notice: "Report was successfully created." }
+        format.html { redirect_to report_report_wizard_index_path(@report) }
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @report.errors, status: :unprocessable_entity }
